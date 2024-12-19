edcb96f151e0718590c277158a94bce0275fca0c
Chloe3c <kristianishenko21@gmail.com>
2024-12-19 21:57:10 +0300

    Program.cs

--- LABA5.1/Program.cs
+++ LABA5.1/Program.cs
@@ -1,22 +1,13 @@
-﻿using System;
+Random random = new Random();
+char[] characters = new char[11];
 
-class Program
+for (int i = 0; i < characters.Length; i++)
 {
-    static void Main()
-    {
-        Random random = new Random();
-        char[] characters = new char[11];
-
-        for (int i = 0; i < characters.Length; i++)
-        {
-            characters[i] = (char)random.Next('A', 'z' + 1);
-        }
-        string randomString = new string(characters);
-        Console.WriteLine($"Случайная строка: {randomString}");
-
-        string cleanedString = randomString.Replace("  ", " ");
-        cleanedString = cleanedString.Trim();
-
-        Console.WriteLine($"Строка без лишних пробелов: {cleanedString}");
-    }
+    characters[i] = (char)random.Next('A', 'z' + 1);
 }
+
+string randomString = new string(characters);
+Console.WriteLine($"Случайная строка: {randomString}");
+
+string cleanedString = randomString.Replace("  ", " ").Trim();
+Console.WriteLine($"Строка без лишни
